ae251d3464e1c88c3791a576f8ff6b599b4ad3b2
Karol Czyżewski <Karolek0493@wp.pl>
2020-09-03 17:40:54 +0200

    add new functions() -> clearMessage, clearPlayground, setDefault

--- main.js
+++ main.js
@@ -1,18 +1,81 @@
 const items = document.querySelectorAll(".playground__item");
+const panel = document.querySelector(".panel__paragraph");
+const button = document.querySelector(".playground__button");
 
-let activePlayer = "X";
+let activePlayer;
 
-const fields = ["", "", "", "", "", "", "", "", ""];
+let gameActive;
+
+let fields;
+
+const setDefault = () => {
+  activePlayer = "X";
+  gameActive = true;
+  fields = ["", "", "", "", "", "", "", "", ""];
+};
+
+setDefault();
+
+const winingConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
+
+const displayWinMessage = () => {
+  panel.innerText = `Gratulacje ${activePlayer}, wygrałeś!`;
+};
+
+const validateGame = () => {
+  for (let i = 0; i <= 7; i++) {
+    const [posA, posB, posC] = winingConditions[i];
+    const value1 = fields[posA];
+    const value2 = fields[posB];
+    const value3 = fields[posC];
+
+    if (value1 !== "" && value1 === value2 && value1 === value3) {
+      gameActive = false;
+      displayWinMessage();
+    }
+  }
+};
 
 items.forEach((item) => {
   item.addEventListener("click", (e) => {
     const { pos } = e.target.dataset;
 
-    if (fields[pos] === "") {
+    if (gameActive && fields[pos] === "") {
       fields[pos] = activePlayer;
 
       e.target.classList.add(`playground__item--field-${activePlayer}`);
+      validateGame();
       activePlayer = activePlayer === "X" ? "O" : "X";
     }
   });
 });
+
+const clearMessage = () => {
+  panel.innerText = "";
+};
+
+const clearPlayground = () => {
+  items.forEach((item) => {
+    item.classList.remove(
+      "playground__item--field-X",
+      "playground__item--field-O"
+    );
+  });
+};
+
+function handleButtonClick() {
+  setDefault();
+  clearMessage();
+  clearPlayground();
+}
+
+button.addEventListener("click", handleButtonClick);
